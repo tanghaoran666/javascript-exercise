78033af886581b8953c8a126a13359f73df09fc4
tanghaoran <407458281@qq.com>
2020-09-21 19:16:48 +0800

    implement all object test

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,7 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const list = Object.values(source);
+  var result = 0;
+  list.forEach(t => (result += Number(t)));
+  return result;
 }
