3749537c63c4c6279da120a40b166a78bc0be845
tanghaoran <407458281@qq.com>
2020-09-21 15:27:16 +0800

    implement 1-string startwith test

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let result = 0;
+  collection.forEach(element => {
+    if (element.indexOf('粤') !== -1) {
+      result++;
+    }
+  });
+  return result;
 }
