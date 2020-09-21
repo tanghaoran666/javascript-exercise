de44f59c4f56ffb2bc084ada7e8568966e060e9b
tanghaoran <407458281@qq.com>
2020-09-21 22:27:20 +0800

    implement 4-class practice_4 test

--- 4-class/practice_4/person.js
+++ 4-class/practice_4/person.js
@@ -1,1 +1,17 @@
+/* eslint-disable class-methods-use-this */
 // TODO 15: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // eslint-disable-next-line camelcase
+  basic_introduce(message) {
+    return message;
+  }
+
+  introduce() {
+    return this.basic_introduce(`My name is ${this.name}. I am ${this.age} years old.`);
+  }
+}
--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,10 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    return super.basic_introduce(
+      `My name is ${this.name}. I am ${this.age} years old. I am a Worker. I have a job.`
+    );
+  }
+}
