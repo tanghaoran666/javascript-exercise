f1b930533dacdbe1bb855041f905535e0f1b833c
tanghaoran <407458281@qq.com>
2020-09-21 22:28:38 +0800

    implement 4-class practice_4 test

--- 4-class/practice_4/student.js
+++ 4-class/practice_4/student.js
@@ -8,6 +8,8 @@ export default class Student extends Person {
   }
 
   introduce() {
-    return `${super.introduce()} I am a Student. I am at Class ${this.klass}.`;
+    return super.basic_introduce(
+      `My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.`
+    );
   }
 }
