451e3895a440d0b29e1ea3cce2d3c78c0a71a002
tanghaoran <407458281@qq.com>
2020-09-21 21:02:35 +0800

    implement class practice-1 test

--- 4-class/practice_2/person.js
+++ 4-class/practice_2/person.js
@@ -1,1 +1,11 @@
 // TODO 10: 在这里写实现代码
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.name}. I am ${this.age} years old.`);
+  }
+}
