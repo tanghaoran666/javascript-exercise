9ec8ca9ff64769470e9bf7524dae69ecca1d9124
tanghaoran <407458281@qq.com>
2020-09-21 17:44:28 +0800

    implement array find test

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,7 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const result = collection.filter(function(item, index, self) {
+    return item.age == 18;
+  });
+  return result[0].name;
 }
