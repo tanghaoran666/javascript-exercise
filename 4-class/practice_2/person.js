f64c297399f1169216f6b940995ff37fa93b8552
tanghaoran <407458281@qq.com>
2020-09-21 21:16:57 +0800

    implement class practice-2 test

--- 4-class/practice_2/person.js
+++ 4-class/practice_2/person.js
@@ -1,11 +1,11 @@
 // TODO 10: 在这里写实现代码
-class Person {
+export default class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
 
   introduce() {
-    console.log(`My name is ${this.name}. I am ${this.age} years old.`);
+    return `My name is ${this.name}. I am ${this.age} years old.`;
   }
 }
