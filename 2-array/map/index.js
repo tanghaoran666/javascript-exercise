cc696eb3346e756cbd72a8d937fc6a66a976a0d1
tanghaoran <407458281@qq.com>
2020-09-21 18:58:12 +0800

    implement array all test

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,6 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const results = collection.map(t => String.fromCharCode(t + 64).toLowerCase());
+
+  return results;
 }
