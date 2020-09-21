7e5c63fa7b0802b7d2570e337e9f5c5adda85231
tanghaoran <407458281@qq.com>
2020-09-21 15:49:45 +0800

    [thr] implement array filter test

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,15 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(s => s % 3 == 0);
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = collection.filter(function(item, index, self) {
+    return self.indexOf(item) == index;
+  });
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
