f8af6e0ef81f5aa8eaba07c0174f5f2c84c9238a
tanghaoran <407458281@qq.com>
2020-09-21 21:20:04 +0800

    implement class practice-3 test

--- 4-class/practice_3/student.js
+++ 4-class/practice_3/student.js
@@ -1,1 +1,12 @@
 // TODO 13: 在这里写实现代码
+import Person from './person';
+export default class Student extends Person {
+  constructor(name, age, className) {
+    super(name, age);
+    this.klass = className;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
+}
