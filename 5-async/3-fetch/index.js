80897278d7ff7e0047286bdfbe8b5fd12dd00563
tanghaoran <407458281@qq.com>
2020-09-22 08:55:20 +0800

    implement 5-async 4-async

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,12 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  fetch(url).then(function(response) {
+    if (response.statusText === 200) {
+      return response.json();
+    }
+    return Promise.reject(new TypeError('error message'));
+  });
   // end -->
 }
 
--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,9 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const result = await fetch(url);
+  const data = await result.json();
+  document.writeln(data.name);
   // end -->
 }
 
